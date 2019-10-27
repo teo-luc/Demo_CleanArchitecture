23fe5cb4802310d2a10244ace7078e5cdb695270
William Nguyen <william@forixwebdesign.com>
2019-10-27 16:52:54 +0700

    Init source code

---
+++ VideoDB_CleanArchitecture/VideoDB_CleanArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoDB_CleanArchitecture
+//
+//  Created by Tèo Lực on 10/27/19.
+//  Copyright © 2019 Luc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
