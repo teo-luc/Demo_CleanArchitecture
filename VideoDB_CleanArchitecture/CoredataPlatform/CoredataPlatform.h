130ed019076627bc0b452e80feb3adfe0e4aa9bb
William Nguyen <william@forixwebdesign.com>
2019-10-27 17:05:48 +0700

    Init targets

---
+++ VideoDB_CleanArchitecture/CoredataPlatform/CoredataPlatform.h
@@ -0,0 +1,19 @@
+//
+//  CoredataPlatform.h
+//  CoredataPlatform
+//
+//  Created by Tèo Lực on 10/27/19.
+//  Copyright © 2019 Luc Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoredataPlatform.
+FOUNDATION_EXPORT double CoredataPlatformVersionNumber;
+
+//! Project version string for CoredataPlatform.
+FOUNDATION_EXPORT const unsigned char CoredataPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoredataPlatform/PublicHeader.h>
+
+
---
+++ VideoDB_CleanArchitecture/NetworkPlatform/NetworkPlatform.h
@@ -0,0 +1,19 @@
+//
+//  NetworkPlatform.h
+//  NetworkPlatform
+//
+//  Created by Tèo Lực on 10/27/19.
+//  Copyright © 2019 Luc Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkPlatform.
+FOUNDATION_EXPORT double NetworkPlatformVersionNumber;
+
+//! Project version string for NetworkPlatform.
+FOUNDATION_EXPORT const unsigned char NetworkPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkPlatform/PublicHeader.h>
+
+
