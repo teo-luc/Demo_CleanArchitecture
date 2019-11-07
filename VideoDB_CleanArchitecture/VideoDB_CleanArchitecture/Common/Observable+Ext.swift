4ceea322887d39504d029fee238cb7b525111d74
Luc Nguyen Tien <lucnguyen@teqnological.asia>
2019-11-07 11:48:23 +0700

    Added an Observable Extension file

--- VideoDB_CleanArchitecture/VideoDB_CleanArchitecture/Common/Observable+Ext.swift
+++ VideoDB_CleanArchitecture/VideoDB_CleanArchitecture/Common/Observable+Ext.swift
@@ -10,35 +10,35 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-//extension ObservableType where Element == Bool {
-//    /// Boolean not operator
-//    public func not() -> Observable<Bool> {
-//        return self.map(!)
-//    }
-//
-//}
-//
-//extension SharedSequenceConvertibleType {
-//    func mapToVoid() -> SharedSequence<SharingStrategy, Void> {
-//        return map { _ in }
-//    }
-//}
-//
-//extension ObservableType {
-//
-//    func catchErrorJustComplete() -> Observable<Element> {
-//        return catchError { _ in
-//            return Observable.empty()
-//        }
-//    }
-//
-//    func asDriverOnErrorJustComplete() -> Driver<Element> {
-//        return asDriver { error in
-//            return Driver.empty()
-//        }
-//    }
-//
-//    func mapToVoid() -> Observable<Void> {
-//        return map { _ in }
-//    }
-//}
+extension ObservableType where E == Bool {
+    /// Boolean not operator
+    public func not() -> Observable<Bool> {
+        return self.map(!)
+    }
+
+}
+
+extension SharedSequenceConvertibleType {
+    func mapToVoid() -> SharedSequence<SharingStrategy, Void> {
+        return map { _ in }
+    }
+}
+
+extension ObservableType {
+
+    func catchErrorJustComplete() -> Observable<E> {
+        return catchError { _ in
+            return Observable.empty()
+        }
+    }
+
+    func asDriverOnErrorJustComplete() -> Driver<E> {
+        return asDriver { error in
+            return Driver.empty()
+        }
+    }
+
+    func mapToVoid() -> Observable<Void> {
+        return map { _ in }
+    }
+}
